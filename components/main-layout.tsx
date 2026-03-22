eb3f730b3f8e02d08d4896c15d9ef3fd2e4eef1a
teramorere-dev <teramorere@gmail.com>
2026-03-22 21:12:10 +0900

    Update main-layout.tsx

--- components/main-layout.tsx
+++ components/main-layout.tsx
@@ -1,3 +1,8 @@
 export function MainLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <h1>My SNS</h1>
+      {children}
+    </div>
+  );
 }
